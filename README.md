208b4b3922219b19e610c2a7f4910ab748ac4afd
srini <srinivasan.magudeeswaran@ottimate.com>
2024-08-09 02:00:00 +0530

    Gitleaks action workflow using python

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gitleaks-actions
-Github actions
+GitHub actions
+
+### Pre Requisites
+* This action will assume that you have already installed Gitleaks actions (will add later)
+* You need to pass ${{ secrets.GITHUB_TOKEN }} as an env to this actions
+* right now supports only push and pull request events
--- src/main.py
+++ src/main.py
@@ -1,40 +1,73 @@
 import os
 import sys
 import json
-
+import subprocess
 import requests
 
 
+class GitLeaksAction:
+    def __init__(self):
+        print("Gitleaks class...")
+        self.base_url = os.environ["GITHUB_API_URL"]
+        self.gh_token = os.environ["GITHUB_TOKEN"]
+        self.event_type = os.environ["GITHUB_EVENT_NAME"]
+        self.owner, self.repo = os.environ["GITHUB_REPOSITORY"].rsplit("/")
+        event_json_path = os.environ["GITHUB_EVENT_PATH"]
 
-def get_commits(event_name="pull_request"):
-    print("Getting commits...")
-    base_url = os.environ["GITHUB_API_URL"]
-    gh_token = os.environ["GITHUB_TOKEN"]
-    event_type = os.environ["GITHUB_EVENT_NAME"]
-    owner, repo = os.environ["GITHUB_REPOSITORY"].rsplit("/")
-    event_json_path = os.environ["GITHUB_EVENT_PATH"]
+        with open(event_json_path, "r") as f:
+            self.gh_events = json.loads(f.read())
 
-    print(base_url, gh_token, event_type, owner, repo, event_json_path)
+        self.headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {self.gh_token}",
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
 
-    json_data = json.load(open(event_json_path))
-    print(json_data)
-    pull_request_number = json_data["number"]
+        self.gitleaks_args = [
+            "detect",
+            "--redact",
+            "-v",
+            "--exit-code=2",
+            "--report-format=sarif",
+            "--report-path=results.sarif",
+            "--log-level=debug",
+        ]
 
-    pr_url = f"{base_url}/repos/{owner}/{repo}/pulls/{pull_request_number}/commits"
+    def get_commits(self):
+        if self.event_type == "pull_request":
+            self.get_pull_request_commits()
+        elif self.event_type == "push":
+            self.get_push_commits()
 
-    headers = {"Accept": "application/vnd.github+json", "Authorization": f"Bearer {gh_token}", "X-GitHub-Api-Version": "2022-11-28"}
-    print(pr_url)
+    def get_pull_request_commits(self):
+        print("Getting pull request commits...")
+        pr_number = self.gh_events["number"]
+        pr_url = (
+            f"{self.base_url}/repos/{self.owner}/{self.repo}/pulls/{pr_number}/commits"
+        )
+        response = requests.get(pr_url, headers=self.headers)
 
-    response = requests.get(url=pr_url, headers=headers)
+        commits = response.json()
 
-    print(response)
-    print("""Boooooooooooo""")
-    print(response.json())
+        scan_info = {
+            "base_ref": commits[0]["sha"],
+            "head_ref": commits[len(commits) - 1]["sha"],
+        }
+        log_cmd = f"--log-opts=--no-merges --first-parent {scan_info['base_ref']}^..{scan_info['head_ref']}"
+        self.gitleaks_args.append(log_cmd)
 
+    def gitleaks_scan(self):
 
+        if self.event_type == "pull_request":
+            self.get_pull_request_commits()
 
+        cmd = f"gitleaks {' '.join(self.gitleaks_args)}"
+        print(cmd)
+        subprocess.run(cmd, shell=True)
+
+    def get_push_commits(self):
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
-    print(sys.argv)
-    get_commits()
+    GitLeaksAction().gitleaks_scan()
