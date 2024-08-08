73ec9d5636effe47af5c622b6676632dc99408f5
srini <srinivasan.magudeeswaran@ottimate.com>
2024-08-08 16:46:01 +0530

    Gitleaks action workflow using python

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 
@@ -7,6 +8,15 @@ def parse_json(json_string):
     print(json.dumps(json_string))
 
 
+def get_commits(event_name="pull_request"):
+    base_url = os.environ["GITHUB_API_URL"]
+    gh_token = os.environ["GITHUB_TOKEN"]
+    event_type = os.environ["GITHUB_EVENT_NAME"]
+    owner, repo = os.environ["GITHUB_REPOSITORY"].rsplit("/")
+
+    print(base_url, gh_token, event_type, owner, repo)
+
+
 if __name__ == "__main__":
     print(sys.argv)
-    parse_json({"Hi": "Hello"})
+    get_commits()
