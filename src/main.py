e7ff7cb4969139e7de2569346187a644d32213ae
srini <srinivasan.magudeeswaran@ottimate.com>
2024-08-09 04:42:19 +0530

    Gitleaks action workflow using python

--- src/main.py
+++ src/main.py
@@ -67,7 +67,7 @@ class GitLeaksAction:
         # tar = tarfile.open(file_path)
         # tar.extractall(path=temp_dir)
         # tar.close()
-        os.system(f"cd {temp_dir} && tar xvfz {filename}")
+        os.system(f"cd {temp_dir} && tar xvf {filename}")
         print(f"Downloaded gitleaks here: {file_path}")
 
         self.gitleaks_bin = os.path.join(temp_dir, "gitleaks")
